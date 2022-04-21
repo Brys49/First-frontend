b68feab2c1db7621e813662ec4d171292aa0a7ee
Brys49 <krzysztof4499@gmail.com>
2022-04-21 21:15:41 +0200

    feat: add start-screen

--- first-frontend/src/app/app-routing.module.ts
+++ first-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartScreenComponent } from './modules/start-screen/start-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/start',
+    pathMatch: 'full'
+  },
+  {
+    path: 'start',
+    component: StartScreenComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
