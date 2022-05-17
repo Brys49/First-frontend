07dbb291d1dfbcc595fdb1fc95ec086354975c5a
Brys49 <krzysztof4499@gmail.com>
2022-05-17 21:46:10 +0200

    build: add Angular Material

--- first-frontend/src/app/app.module.ts
+++ first-frontend/src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartScreenComponent } from './modules/start-screen/start-screen.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { HeaderComponent } from './modules/shared_components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     StartScreenComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
