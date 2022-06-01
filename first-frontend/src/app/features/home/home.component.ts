705b466f72a2ac379fe355b42e6871358ad5aacd
Brys49 <krzysztof4499@gmail.com>
2022-06-01 18:20:03 +0200

    feat: add links inside sidenav

--- first-frontend/src/app/features/home/home.component.ts
+++ first-frontend/src/app/features/home/home.component.ts
@@ -7,7 +7,6 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
   constructor(private router: Router) { }
 
   ngOnInit(): void {
