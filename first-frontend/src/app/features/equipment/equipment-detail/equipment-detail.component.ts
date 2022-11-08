fa33143bebdeb43e9c4ca6f587bb01dd14c66f2c
Brys49 <krzysztof4499@gmail.com>
2022-11-08 10:37:55 +0100

    feat: display details of equipment

--- first-frontend/src/app/features/equipment/equipment-detail/equipment-detail.component.ts
+++ first-frontend/src/app/features/equipment/equipment-detail/equipment-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Equipment } from 'src/app/core/models/equipment.model';
+import { EquipmentService } from 'src/app/core/services/equipment.service';
 
 @Component({
   selector: 'app-equipment-detail',
@@ -6,10 +9,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment-detail.component.scss']
 })
 export class EquipmentDetailComponent implements OnInit {
+  private equipmentId: number = 0;
+  public equipment!: Equipment;
+  public listContent: Map<string, any> = new Map();
+  public listContentKeys: string[] = [];
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private equipmentService: EquipmentService
+  ) { }
 
   ngOnInit(): void {
+    this.equipmentId = Number(this.route.snapshot.paramMap.get('id'));
+    this.getEquipment();
+  }
+
+  private getEquipment(): void {
+    this.equipmentService.getEquipment(this.equipmentId).subscribe(equipment => this.equipment = equipment);
+
+    if (this.equipment) {
+      this.generateContent();
+    } else {
+      this.goBack();
+    }
+  }
+
+  private generateContent(): void {
+    this.listContent.clear();
+    this.listContent.set("Serial number", this.equipment.serialNumber);
+    this.listContent.set("Price", this.equipment.price);
+    for (let paramKey of this.equipment.parameters.keys()) {
+      this.listContent.set(paramKey, this.equipment.parameters.get(paramKey));
+    }
+    this.listContentKeys = Array.from(this.listContent.keys());
+  }
+
+  public goBack(): void {
+    this.router.navigateByUrl('/home/equipment');
+  }
+
+  public deleteEquipment(id: number): void {
+    this.equipmentService.deleteEquipment(id);
+    this.goBack();
   }
 
 }
